8d55c9a7a7fd081124a2f2dfd8a77c09e79aa5a3
yusran234 <teukuyusransyahtandi@gmail.com>
2018-07-29 12:56:48 +0700

    ini adalah commit pertama

---
+++ helloworld.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Belajar Git - Project 01</title>
+    </head>
+    <body>
+		<marquee> <h1> JAVA IS MEH ..     JAVA IS MEH ..      JAVA IS MEH ..    JAVA IS MEH .. </h1> </marquee>
+        <p>Hello Semua, Saya sedang belajar Git</p>
+
+    </body>
+</html>
